0dabb12c6ed7ebe14e775066115771d0fcc2fcdc
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-11-08 15:17:39 +0700

    edit dummy data

--- src/db/categories.ts
+++ src/db/categories.ts
@@ -29,25 +29,25 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: "4d7b1648-eaac-4224-b140-a701effe61df",
-    type: "smart watches",
+    type: "Smart Watches",
     src: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main1_0ae2597d-dee4-42fd-9a18-eb4ae0b3bc43_420x.png?v=1647765304",
     alt: "smart watches",
   },
   {
     _id: "138ef8ab-54d3-47f6-a4cb-a3a71412ec9e",
-    type: "wired earphones",
+    type: "Wired Earphones",
     src: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/103blk_420x.png?v=1613457250",
     alt: "earphones",
   },
   {
     _id: "3d3d656f-2077-4734-b8da-50ecc7d09067",
-    type: "wireless earphones",
+    type: "Wireless Earphones",
     src: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/rockerz-255-pro-red_bd675504-5750-4b42-97b9-79caf2cc9034_420x.png?v=1614587254",
     alt: "wireless earphones",
   },
   {
     _id: "b9609e4e-30a8-4ad4-86f0-a9cde717bcac",
-    type: "TWS earphones",
+    type: "TWS Earphones",
     src: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/218997d1-3fe9-4a19-809c-94a8a2d05273_600x.png?v=1625045659",
     alt: "TWS earphones",
   },
@@ -59,7 +59,7 @@ export const categories = [
   },
   {
     _id: "16d8df9a-04e8-4373-a496-ad9728fb377d",
-    type: "speakers",
+    type: "Speakers",
     src: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main2_8549ad38-acec-45d6-bba4-8b202a9bfdc1_420x.png?v=1646976976",
     alt: "speakers",
   },
