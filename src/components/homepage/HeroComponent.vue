f5563e802512df0753bd3e0de0257ad515af6715
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-11-08 16:03:21 +0700

    edit hero component size

--- src/components/homepage/HeroComponent.vue
+++ src/components/homepage/HeroComponent.vue
@@ -3,7 +3,7 @@ import { heroImages } from "../../data";
 </script>
 
 <template>
-  <el-carousel class="hero" trigger="click" height="500px">
+  <el-carousel class="hero" trigger="click" height="550px">
     <el-carousel-item v-for="item in heroImages" :key="item.id">
       <img :src="item.src" :alt="item.alt" />
     </el-carousel-item>
