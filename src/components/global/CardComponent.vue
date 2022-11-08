09e5ccf073e759fd1b22d763f20cf7d4d1e39c15
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-11-08 15:16:58 +0700

    add card component

---
+++ src/components/global/CardComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+div {
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+}
+</style>
