60f5bd71ac8d79439b72028da3f2cf21d14f1d9e
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-11-08 15:17:26 +0700

    add hero component

---
+++ src/components/homepage/HeroComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { heroImages } from "../../data";
+</script>
+
+<template>
+  <el-carousel class="hero" trigger="click" height="500px">
+    <el-carousel-item v-for="item in heroImages" :key="item.id">
+      <img :src="item.src" :alt="item.alt" />
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<style scoped>
+img {
+  max-width: 100%;
+  max-height: 100%;
+}
+</style>
