be5f06eca084c5c45ead33ea94d07a8588c61b05
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-11-09 15:29:53 +0700

    add product list

---
+++ src/components/Products/ProductList.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import ProductCard from "./ProductCard.vue";
+import { products } from "@/db";
+</script>
+
+<template>
+  <div class="product-list-wrapper layout-wrapper">
+    <ProductCard
+      v-for="product in products"
+      :key="product._id"
+      :name="product.productTitle"
+      :price="product.productPrice"
+      :image="product.imgUrl"
+      :discount="product.productDiscpercent"
+      :og-price="product.productOgPrice"
+    />
+  </div>
+</template>
+
+<style>
+.product-list-wrapper {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  column-gap: 1.5rem;
+  row-gap: 1.5rem;
+}
+</style>
