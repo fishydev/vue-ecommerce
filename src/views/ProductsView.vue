e684a17b033e0d30d1824a83c91430b9a6cf1ea2
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-11-09 15:30:29 +0700

    add products page

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import FilterDrawer from "@/components/Products/FilterDrawer.vue";
+import ProductList from "@/components/Products/ProductList.vue";
+</script>
+
+<template>
+  <div class="products-view">
+    <FilterDrawer />
+    <ProductList />
+  </div>
+</template>
+
+<style>
+.products-view {
+  display: flex;
+}
+</style>
