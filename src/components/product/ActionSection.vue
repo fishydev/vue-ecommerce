27cc4ace4d9f4e00a819c5fe58df9749fcd709d5
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-11-10 17:10:21 +0700

    add product detail components

---
+++ src/components/product/ActionSection.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ElButton } from "element-plus";
+defineProps<{
+  image: string;
+  alt: string;
+}>();
+</script>
+
+<template>
+  <section>
+    <img :src="image" :alt="alt" />
+    <ElButton
+      class="btn-add-to-cart"
+      size="large"
+      type="primary"
+      plain
+      bg
+      color="#000000"
+      >Add to Cart</ElButton
+    >
+  </section>
+</template>
+
+<style scoped>
+img {
+  height: 25rem;
+}
+section {
+  display: flex;
+  flex-direction: column;
+}
+
+.btn-add-to-cart {
+  width: 10rem;
+  align-self: center;
+  text-transform: uppercase;
+  border: 1px solid black;
+  border-radius: 2px;
+}
+</style>
---
+++ src/components/product/DetailSection.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { ElRate } from "element-plus";
+const props = defineProps<{
+  name: string;
+  brand: string;
+  description: string;
+  price: number;
+  rating: number;
+  review: number;
+}>();
+
+const rating = computed(() => {
+  return props.rating;
+});
+</script>
+
+<template>
+  <section>
+    <h1>{{ name }}</h1>
+    <span class="brand">{{ `By ${brand}` }}</span>
+    <p>{{ description }}</p>
+    <span>{{ `Rs. ${price}` }}</span>
+    <div class="product-score">
+      <ElRate v-model="rating" disabled="" />
+      <span>{{ `${review} Reviews` }}</span>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+section {
+  display: flex;
+  flex-direction: column;
+  width: 30rem;
+}
+
+h1 {
+  margin: 0;
+  font-size: xx-large;
+}
+
+.brand {
+  text-transform: uppercase;
+  margin-bottom: 1rem;
+  font-size: large;
+  font-weight: 300;
+}
+
+.product-score {
+  display: flex;
+  align-items: center;
+}
+
+.product-score span {
+  text-transform: capitalize;
+  font-weight: bold;
+  font-size: small;
+}
+</style>
