afab462dffc49151f5fcfa50a17e4b2d4bd64dd9
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-11-10 17:13:18 +0700

    remove template files

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,14 +21,14 @@ const router = createRouter({
       name: "product-detail",
       component: ProductDetailView,
     },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
+    // {
+    //   path: "/about",
+    //   name: "about",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import("../views/AboutView.vue"),
+    // },
   ],
 });
 
