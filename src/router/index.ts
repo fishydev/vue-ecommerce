2204ed579577ff0b2a4c9063772fd2c35f1de7fa
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-11-10 17:09:32 +0700

    add detail page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ProductsView from "@/views/ProductsView.vue";
+import ProductDetailView from "@/views/ProductDetailView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/products",
       name: "products",
       component: ProductsView,
+    },
+    {
+      path: "/detail",
+      name: "product-detail",
+      component: ProductDetailView,
     },
     {
       path: "/about",
---
+++ src/views/ProductDetailView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import DetailSection from "@/components/product/DetailSection.vue";
+import ActionSection from "@/components/product/ActionSection.vue";
+import { products } from "@/db";
+
+const product = ref(products[0]);
+</script>
+
+<template>
+  <div class="detail-view">
+    <DetailSection
+      :name="product.productTitle"
+      :brand="product.brand"
+      :description="product.productDesc"
+      :price="product.productPrice"
+      :rating="product.starRating"
+      :review="product.reviews"
+    />
+    <ActionSection :image="product.imgUrl" :alt="product.alt" />
+  </div>
+</template>
+
+<style>
+.detail-view {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  justify-content: space-around;
+  column-gap: 2rem;
+}
+</style>
