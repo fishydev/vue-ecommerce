064cc0ee5f3f30b0cb7ee10f3baf83914084d8a5
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-11-23 16:47:38 +0700

    setup axios

---
+++ src/lib/axios.ts
@@ -0,0 +1,34 @@
+import Axios, { type AxiosInstance } from "axios";
+import { ElNotification } from "element-plus";
+
+const baseUrl = "http://localhost:3000/api/v1";
+
+const axios: AxiosInstance = Axios.create({
+  baseURL: baseUrl,
+  timeout: 2000,
+});
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.data) {
+      ElNotification({
+        title: "Error",
+        message: `${error.message.data} (${error.response})`,
+        type: "error",
+      });
+    } else {
+      ElNotification({
+        title: "Error",
+        message: error,
+        type: "error",
+      });
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default axios;
