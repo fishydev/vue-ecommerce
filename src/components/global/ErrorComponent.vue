eed584c111c7e4f0a3eae8062aafcb7f28700e45
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-11-25 16:16:11 +0700

    error load component

---
+++ src/components/global/ErrorComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  message?: string;
+}>();
+</script>
+
+<template>
+  <div class="error-container">
+    <h2>
+      {{ `Something unexpected happened.${message ? ` (${message})` : ``}` }}
+    </h2>
+  </div>
+</template>
+
+<style>
+.error-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 60vh;
+}
+</style>
