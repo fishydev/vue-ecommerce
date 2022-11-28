78342594c8eb6d92cade7e43ac31b47b92b23328
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-11-28 12:41:53 +0700

    product detail page: - add loading view - on error view

--- src/views/ProductDetailView.vue
+++ src/views/ProductDetailView.vue
@@ -4,23 +4,27 @@ import { useRoute } from "vue-router";
 import DetailSection from "@/components/product/DetailSection.vue";
 import ActionSection from "@/components/product/ActionSection.vue";
 import ErrorComponent from "@/components/global/ErrorComponent.vue";
+import LoadingComponent from "@/components/global/LoadingComponent.vue";
 import { getProductByUuid } from "@/api/products";
 import type { Product } from "@/types";
 
 const productData = ref<Product>();
-const loading = ref(true);
+const isLoading = ref(true);
+const isError = ref(false);
+const errorMessage = ref("");
 
 const route = useRoute();
 
 const fetchProductData = async (uuid: string) => {
   try {
-    loading.value = true;
+    isLoading.value = true;
     const result = (await getProductByUuid(uuid)).data;
     productData.value = result;
-  } catch (error) {
-    //
+  } catch (error: any) {
+    isError.value = true;
+    if (error.message) errorMessage.value = error.message;
   } finally {
-    loading.value = false;
+    isLoading.value = false;
   }
 };
 
@@ -30,18 +34,23 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="detail-view" v-if="productData">
-    <DetailSection
-      :name="productData.productTitle"
-      :brand="productData.brand"
-      :description="productData?.description"
-      :price="productData.price"
-      :rating="productData.rating"
-      :review="productData.reviews"
-    />
-    <ActionSection :image="productData.imageUrl" :alt="productData.alt" />
+  <div v-if="isLoading" class="loading-wrapper">
+    <LoadingComponent />
   </div>
-  <ErrorComponent v-else />
+  <div v-else>
+    <ErrorComponent :message="errorMessage" v-if="isError" />
+    <div class="detail-view" v-if="productData && !isError">
+      <DetailSection
+        :name="productData.productTitle"
+        :brand="productData.brand"
+        :description="productData.description"
+        :price="productData.price"
+        :rating="productData.rating"
+        :review="productData.reviews"
+      />
+      <ActionSection :image="productData.imageUrl" :alt="productData.alt" />
+    </div>
+  </div>
 </template>
 
 <style>
@@ -51,5 +60,9 @@ onMounted(async () => {
   height: 100%;
   justify-content: space-around;
   column-gap: 2rem;
+}
+
+.loading-wrapper {
+  height: 50vh;
 }
 </style>
