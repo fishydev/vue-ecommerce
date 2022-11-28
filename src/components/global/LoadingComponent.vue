0eb8e96ed044a20b9e73f6c86c48aad33720fd9d
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-11-28 12:42:24 +0700

    add loading component

---
+++ src/components/global/LoadingComponent.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Loading from "vue-loading-overlay";
+defineProps<{
+  variant?: {
+    type: "spinner" | "dots" | "bars";
+    default: "spinner";
+  };
+}>();
+
+const isLoading = true;
+</script>
+
+<template>
+  <div>
+    <Loading :loader="variant" v-model:active="isLoading" />
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+}
+</style>
