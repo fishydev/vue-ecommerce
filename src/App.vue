44aca12c667b2ffb5639cd7e6887bfdfdf9fb8ca
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-11-28 12:42:09 +0700

    remove unused component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 import NavbarComponentVue from "./components/global/NavbarComponent.vue";
 </script>
 
