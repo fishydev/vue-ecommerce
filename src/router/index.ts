d653159661a425b29ed0c09cf31ec7d0463fb538
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-12-06 16:25:20 +0700

    checkout view route

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ProductsView from "@/views/ProductsView.vue";
 import ProductDetailView from "@/views/ProductDetailView.vue";
+import CheckoutView from "@/views/CheckoutView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: "/product/:uuid",
       name: "product-detail",
       component: ProductDetailView,
+    },
+    {
+      path: "/checkout",
+      name: "checkout",
+      component: CheckoutView,
     },
     // {
     //   path: "/about",
